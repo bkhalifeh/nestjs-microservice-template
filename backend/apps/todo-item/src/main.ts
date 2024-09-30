77f2ff75b689d9f2db59352b06f68b530d9ab515
Behzad Khalifeh <bkhalifeh@protonmail.com>
2024-09-30 21:18:51 +0330

    refactor: setup microservice todo-item

--- backend/apps/todo-item/src/main.ts
+++ backend/apps/todo-item/src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { TodoItemModule } from './todo-item.module';
+import { appInit, createOnListen } from '@app/shared';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(TodoItemModule);
-  await app.listen(3000);
+  const { app, logger } = await appInit(AppModule);
+  await app.startAllMicroservices();
+  await app.listen(3000, '0.0.0.0', createOnListen(logger));
 }
 bootstrap();
