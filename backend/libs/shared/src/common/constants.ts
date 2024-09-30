23d1b98216b9887a16e522cd3e22a8c6c939b637
Behzad Khalifeh <bkhalifeh@protonmail.com>
2024-09-30 15:39:22 +0330

    feat: var for jwtModule configuration

--- backend/libs/shared/src/common/constants.ts
+++ backend/libs/shared/src/common/constants.ts
@@ -1,1 +1,16 @@
+import { JwtModule } from '@nestjs/jwt';
+import { UtilModule } from '../modules/util/util.module';
+import { ConfigService } from '@nestjs/config';
+import { UtilService } from '../modules/util/services/util.service';
+
 export const CLIENT_NATS = 'CLIENT_NATS';
+export const jwtModule = JwtModule.registerAsync({
+  imports: [UtilModule],
+  inject: [ConfigService, UtilService],
+  useFactory: (configService: ConfigService, utilService: UtilService) => {
+    return {
+      secret: utilService.b64decode(configService.get('JWT_SECRET', '')),
+      signOptions: { expiresIn: '7d' },
+    };
+  },
+});
