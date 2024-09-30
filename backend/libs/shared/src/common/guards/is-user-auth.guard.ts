dec7113c7583328f8f8423e063e69a7786e63419
Behzad Khalifeh <bkhalifeh@protonmail.com>
2024-09-30 12:10:16 +0330

    feat: add guard to check user auth

---
+++ backend/libs/shared/src/common/guards/is-user-auth.guard.ts
@@ -0,0 +1,44 @@
+import { UtilService } from '@app/shared/modules';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class IsUserAuth implements CanActivate {
+  private readonly secret: Buffer;
+  constructor(
+    private readonly jwtService: JwtService,
+    configService: ConfigService,
+    utilService: UtilService,
+  ) {
+    this.secret = utilService.b64decode(configService.get('JWT_SECRET', ''));
+  }
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = this.jwtService.verify(token, {
+        secret: this.secret,
+      });
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
